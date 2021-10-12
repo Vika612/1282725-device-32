57673c2a61e57dd03f886998f023c01dfcfa7167
Victoria Domovets <v.domovets@gmail.com>
2021-10-12 20:38:20 +0200

    реализация табов js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*  SLIDER */
 
 let position = 0;
@@ -81,3 +83,29 @@ const checkBtns = () => {
 };
 
 checkBtns();
+
+
+/* TABS */
+
+const tabNavs = document.querySelectorAll(".button--tab");
+const tabPanes = document.querySelectorAll(".tabs__item-content");
+
+for (let i = 0; i < tabNavs.length; i++) {
+
+  tabNavs[i].addEventListener("click", function(e){
+    e.preventDefault();
+    const activeTabAttr = e.target.getAttribute("data-tab");
+
+    for (let j = 0; j < tabNavs.length; j++) {
+      const contentAttr = tabPanes[j].getAttribute("data-tab-content");
+
+      if (activeTabAttr === contentAttr) {
+        tabNavs[j].classList.add("active");
+        tabPanes[j].classList.add("active");
+      } else {
+        tabNavs[j].classList.remove("active");
+        tabPanes[j].classList.remove("active");
+      }
+    };
+  });
+}
